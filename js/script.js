73d82a2e9baa4c8b7c3d16c690823289a0bd580c
Nvard Asatryan <nvard.asatryan96@gmail.com>
2022-01-10 21:52:32 +0400

    asas

--- js/script.js
+++ js/script.js
@@ -8,3 +8,34 @@ $(document).ready(function() {
         $(this).closest(".accordion__item").toggleClass("open-accordion");
     });
 });
+///
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+$(document).ready(function() {
+    $('.header-modal div').click(function() {
+        $('.header-modal div').removeClass('tab-active');
+        $(this).addClass('tab-active');
+    });
+});
